fe2c972240a9ba9f22f0c59d460fb3fc196dcb39
hekailing <1034716312@qq.com>
2016-03-06 16:07:14 +0800

    add mysql operation

--- account_info.py
+++ account_info.py
@@ -1,4 +1,34 @@
 #!/usr/bin/python
+# encoding=utf-8
+import string
+import random
+import MySQLdb
+
+import genhash
+
+
+_rootPassword = '123456789'
+_avatardb = 'avatar_test'
+
+
+def execQuery(query, param=None, fetch=False):
+    '''
+    Open MySQLdb and execute the query.  Fetch result if necessary.  
+    Close cursor and database at last.  
+    Return False if execution failed, otherwise True or fetchdata.
+    This function only tested with 'select', 'insert', 'update'
+    '''
+    db = MySQLdb.connect('localhost', 'root', _rootPassword, _avatardb, charset='utf8')
+    cursor = db.cursor()
+    try:
+        cursor.execute(query, param)
+    except MySQLdb.IntegrityError:
+        data = False
+    else:
+        data = cursor.fetchone() if fetch else db.commit() is None
+    cursor.close()
+    db.close()
+    return data
 
 
 def accountCheck(username, password):
@@ -7,21 +37,120 @@ def accountCheck(username, password):
     return: error message.
     '''
     if username and password:
-        return True
+        sql = 'SELECT password, salt FROM user_info WHERE username=%s'
+        ret = execQuery(sql, (username,), True)
+        if isinstance(ret, tuple):
+            saltedPassword, salt = ret
+            return saltedPassword == genhash.genSha256(salt+password)
+        else:
+            return False
     else:
         return False
 
 
-def insertAccount():
-    pass
+def usernameExist(username):
+    if username:
+        sql = 'SELECT * FROM user_info WHERE username=%s'
+        # return execQuery(sql, (username,), True) is not None
+        ret = execQuery(sql, (username,), True)
+        print sql
+        print ret
+        return ret is not None
+    else:
+        return False
 
 
-def updatePicture():
-    pass
+def emailExist(email):
+    if email:
+        sql = 'SELECT * FROM email2user WHERE email=%s'
+        # return execQuery(sql, (email,), True) is not None
+        ret = execQuery(sql, (email,), True)
+        print sql
+        print ret
+        return ret is not None
+    else:
+        return False
 
 
-def getPicture():
-    pass
+def insertAccount(username, password, email):
+    '''
+    Insert an account into database.  The function includes two steps:
+    1. insert a record into user_info(username, password, salt, email)
+    2. insert a record into email2user(email, username)
+    These two step should in a transaction, so DO NOT call execQuery seperatedly
+    '''
+    digitalpha = string.digits + string.lowercase
+    salt = ''.join([random.choice(digitalpha) for i in range(64)])
+    saltedPassword = genhash.genSha256(salt+password)
+    db = MySQLdb.connect('localhost', 'root', _rootPassword, _avatardb, charset='utf8')
+    cursor = db.cursor()
+    insertUserSql = ('INSERT INTO user_info (username, password, salt, email) '
+                     'VALUES (%s, %s, %s, %s)')
+    insertEmailSql = 'INSERT INTO email2user (email, username) VALUES (%s, %s)'
+    try:
+        cursor.execute(insertUserSql, (username, saltedPassword, salt, email))
+        cursor.execute(insertEmailSql, (email, username))
+    except MySQLdb.IntegrityError:
+        ret = False
+    else:
+         db.commit()
+         ret = True
+    cursor.close()
+    db.close()
+    return ret
+
+        
+# discard
+# def updatePicture(email, picurl):
+#     emailHash = genhash.genMd5(email)
+#     sql = 'UPDATE email2pic SET picurl = %s WHERE email = %s'
+#     execQuery(sql, (picurl, emailHash))
 
 
+# discard
+# def getPictureURLFromEmail(emailHash):
+#     sql = 'SELECT picurl FROM email2pic WHERE email = %s'
+#     return execQuery(sql, (emailHash,))
+ 
 
+def main():
+    # userinfo
+    username = 'hekai'
+    password = '123456'
+    email = 'hekai@163.com'
+    # clear the user_info
+    execQuery('DELETE FROM user_info')
+    execQuery('DELETE FROM email2user')
+    insertAccount(username, password, email)
+    if usernameExist(username):
+        print '[OK] username(%s) exist' % username
+    else:
+        print '[FAILED] username(%s) not exist' % username
+    if not usernameExist('unexistedUsername'):
+        print '[OK] username(unexistedUsername) not exist'
+    else:
+        print '[FAILED] username(unexistedUsername) exist'
+    if emailExist(email):
+        print '[OK] email(%s) exist' % email
+    else:
+        print '[FAILED] email(%s) not exist' % email
+    if not emailExist('unexistedEmail@163.com'):
+        print '[OK] email(unexistedEmail@163.com) not exist'
+    else:
+        print '[FAILED] email(unexistedEmail@163.com) exist'
+    if accountCheck(username, password):
+        print '[OK] accountCheck success'
+    else:
+        print '[FAILED] accountCheck failure'
+    if not accountCheck(username, 'wrong password'):
+        print '[OK] wrong password failure'
+    else:
+        print '[FAILED] wrong password passed'
+    if not accountCheck('unexistUsername', password):
+        print '[OK] unexistUsername failure'
+    else:
+        print '[FAILED] unexistUsername passed'
+
+
+if __name__ == '__main__':
+    main()
--- create.py
+++ create.py
@@ -1,11 +1,41 @@
 #! /usr/bin/python
 import cgi
 import cgitb
+import re
 
 from  page_template import readPageTmpl as readPageTmpl
 import account_info
 
 
+def usernameCheck(username):
+    '''
+    Check the username format:
+    length is not greater than 20
+    only including alphabet, digit and _
+    '''
+    if len(username) > 20:
+        return False
+    else if re.match('^\w+$', username):
+        return True
+    else:
+        return False
+
+
+def emailCheck(email):
+    ''' 
+    Check the email format    
+    length is between 8 and 32
+    and the email is like '\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+'
+    '''
+    emailLen = len(email)
+    if emailLen > 32 or emailLen <= 7:
+        return False
+    else if re.match('^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$', email):
+        return True
+    else:
+        return False
+
+    
 cgitb.enable()
 fieldData = cgi.FieldStorage()
 print "Content-Type:text/html\n"
@@ -17,7 +47,23 @@ if 'create' in fieldData:
     password = fieldData.getvalue('password')
     email = fieldData.getvalue('email')
     if username and password and email:
-        print loginForm.replace('ErrorMsg', '')
+        if not usernameCheck(username):
+            errorMsg = 'username format error'
+        else if not emailCheck(email):
+            errorMsg = 'email format error'
+        else if account_info.usernameExist(username):
+            errorMsg = 'username is already used'
+        else if account_info.emailExist(email):
+            errorMsg = 'email is already used'
+        else if account_info.insertAccount(username, password, email):
+            errorMsg = ''
+        else:
+            errorMsg = 'database operation failure'
+            errorMsg = 'username or password is wrong'
+        if errorMsg:
+            print createForm.replace('ErrorMsg', errorMsg)
+        else:
+            print loginForm.replace('ErrorMsg', '')
     else:
         print createForm.replace('ErrorMsg', 'Please fill in all information')
 else:
